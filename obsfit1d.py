70fc69490fa8ecddd225c721596622375862318f
Ryan Holmes <holmes.ryan.m@gmail.com>
2018-07-13 12:13:40 +1000

    Added error estimate from least-squares fit.

--- obsfit1d.py
+++ obsfit1d.py
@@ -15,19 +15,21 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 # Grid parameters:
-Lh = 4000. # Max size of h-grid
-n = 200 # Number of h-points
+n = 100 # Number of h-points
+hper = 0.99 # High-percentile
+lper = 0.01 # Low-percentile
+buf = 500. # Buffer (m)
 dt = int(1e5) # Time-step
-
-h = np.linspace(-Lh/2.,Lh/2.,n)
-dh = h[1]-h[0]
 
 # Initial guess:
 # [K0, Kh, w]
-x0 = [1., 0., 0.]
+x0 = [1., 0., 1.]
 
 # Scales
 xsc = [1.e-4,1.e-7,1.e-7]
+
+# Method:
+method = 0 # 0 = leastsq, 1 = minimize
 
 ## Least Squares method:
 def fit3par(zF,trF,tf,sz):
@@ -36,60 +38,54 @@ def fit3par(zF,trF,tf,sz):
     tracer distribution is a gaussian about zF=0 with standard
     deviation sz"""
 
-
-    trI,tmp = trINI(sz)
-    trO,tmp = trOBS(zF,trF)
+    h = get_grid(zF,trF)
+    trI = trINI(sz,h)
+    trO = trOBS(zF,trF,h)
 
     nt = tf // dt
-    res,success = leastsq(cost, x0, args = (nt,trI,trO))
-    res[0] = res[0]*xsc[0]
-    res[1] = res[1]*xsc[1]
-    res[2] = res[2]*xsc[2]
+    if (method == 0):
+        res = leastsq(cost, x0, args= (nt,trI,trO,h),full_output=1)
+        class resout:
+            pass
+        reso = resout()
+        reso.fun = costCHISQ(res[0], nt, trI, trO, h)
+        reso.x = [res[0][0]*xsc[0],res[0][1]*xsc[1],res[0][2]*xsc[2]]
+        reso.success = res[-1]
+        pcov = res[1] * ( reso.fun / (len(h)-3))
+        reso.xerr = []
+        for i in range(3):
+            reso.xerr.append(xsc[i]*np.absolute(pcov[i][i])**0.5)
+        res = reso
+    else:
+        res = minimize(costCHISQ, x0, args = (nt,trI, trO, h), method = 'nelder-mead',
+                       options = {'xtol': 1e-5})
+        res.x[0] = res.x[0]*xsc[0]
+        res.x[1] = res.x[1]*xsc[1]
+        res.x[2] = res.x[2]*xsc[2]
     
     return(res)
 
-def cost(x, nt, trI, trO):
+def cost(x, nt, trI, trO,h):
     """ Cost function """
 
-    tr = solve(x[0]*xsc[0],x[1]*xsc[1],x[2]*xsc[2],nt,trI)
+    tr = solve(x[0]*xsc[0],x[1]*xsc[1],x[2]*xsc[2],nt,trI,h)
     
     return(tr-trO)
 
-
-## Minimize method:
-def fit3parMN(zF,trF,tf,sz):
-    """Return parameters K0, Kh and w that best match the tracer
-    profile trF as a function of zF at time tf where the initial
-    tracer distribution is a gaussian about zF=0 with standard
-    deviation sz"""
-
-
-    trI,tmp = trINI(sz)
-    trO,tmp = trOBS(zF,trF)
-
-    nt = tf // dt
-    res = minimize(costMN, x0, args = (nt,trI, trO), method = 'nelder-mead',
-                   options = {'xtol': 1e-4, 'disp': True})
-    res.x[0] = res.x[0]*xsc[0]
-    res.x[1] = res.x[1]*xsc[1]
-    res.x[2] = res.x[2]*xsc[2]
-    
-    return(res)
-
-def costMN(x, nt, trI, trO):
+def costCHISQ(x, nt, trI, trO, h):
     """ Cost function """
-
-    tr = solve(x[0]*xsc[0],x[1]*xsc[1],x[2]*xsc[2],nt,trI)
+    tr = solve(x[0]*xsc[0],x[1]*xsc[1],x[2]*xsc[2],nt,trI,h)
     
-    return(chisq(tr,trO))
+    chisq = np.sum((tr-trO)**2)
+    return(chisq)
 
-def solve(K0,Kh,w,nt,trI):
+def solve(K0,Kh,w,nt,trI,h):
     """ Take an initial concentration to a final concentration."""
-
+    
     K = np.maximum( K0 + h*Kh , np.zeros_like(h) )
     A = csr_matrix(np.tile(K,(n,1)).T)
-    A = A.multiply(D2())
-    A = A + (Kh - w) * D1()
+    A = A.multiply(D2(h[1]-h[0]))
+    A = A + (Kh - w) * D1(h[1]-h[0])
 
     tr = np.copy(trI)
     for t in range(nt):
@@ -102,9 +98,10 @@ def plot(x,zF,trF,tf,sz):
     parameters."""
 
     # Plot modelled and observed solutions:
-    trI, h = trINI(sz)
-    trO, h = trOBS(zF,trF)
-    trM = solve(x[0],x[1],x[2],tf // dt, trI)
+    h = get_grid(zF,trF)
+    trI = trINI(sz,h)
+    trO = trOBS(zF,trF,h)
+    trM = solve(x[0],x[1],x[2],tf // dt, trI, h)
 
     f = plt.figure(figsize=(5,5),facecolor='white')
     plt.plot(trI,h,'-r',label='Initial')
@@ -114,11 +111,22 @@ def plot(x,zF,trF,tf,sz):
     plt.xlabel('Tracer Concentration')
     plt.ylabel('Depth above target surface')
     plt.xlim([0., np.max(trO)*1.1])
-    plt.ylim([-Lh/2.,Lh/2.])
-    plt.title('$\kappa_0= $ %.2e, $\kappa_h = $ %.2e, $w = $, %.2e'  % (x[0],x[1],x[2]))
+    plt.ylim([np.min(h),np.max(h)])
+    plt.title('Time = %04d, $\kappa_0= $ %.2e, $\kappa_h = $ %.2e, $w = $, %.2e'  % (tf // 1e5, x[0],x[1],x[2]))
     
-def trOBS(zF,trF):
+def get_grid(zF,trF):
+    """ Determine h-grid to use"""
+    csum = np.cumsum(trF)
+    f = interp1d(csum,zF)
+    hmax = f(hper*csum[-1])+buf
+    hmin = f(lper*csum[-1])-buf
+    
+    h = np.linspace(hmin,hmax,n)
+    return(h)
+
+def trOBS(zF,trF,h):
     """ Return observed final tracer distribution on h grid"""
+    dh = h[1]-h[0]
     f = interp1d(zF,trF,fill_value=0.)
     trO = np.zeros_like(h)
     in_range = np.logical_and(h>=np.min(zF),h <= np.max(zF))
@@ -126,22 +134,17 @@ def trOBS(zF,trF):
 
     trO = trO / np.sum(trO*dh)
 
-    return (trO,h)
+    return (trO)
 
-def trINI(sz):
+def trINI(sz,h):
     """ Return initial tracer distribution """
+    dh = h[1]-h[0]
     trI = np.exp(-h**2/2/sz**2)
     trI = trI / np.sum(trI*dh)
 
-    return (trI,h)
+    return (trI)
 
-def chisq(f1,f2):
-    """Calculated L2 norm of difference between two concentration
-    profiles"""
-    chisq = np.sum((f1-f2)**2)
-    return(chisq)
-
-def D2():
+def D2(dh):
     """Centered 2nd derivative matrix"""
     ec=([-1.] + (n-2)*[-2.] + [-1.])
     em=[1.]*n
@@ -150,7 +153,7 @@ def D2():
     A/=dh**2
     return A
 
-def D1():
+def D1(dh):
     """Centered 1st derivative matrix"""
     ec=([1] + (n-2)*[0] + [-1.])
     em=[1.]*n
